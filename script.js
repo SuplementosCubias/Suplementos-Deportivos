ff9c213458cf7d68a196b215cd5f57e04ffd46df
SuplementosCubias <cubiasonline@gmail.com>
2026-06-29 18:17:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const telefono = "50376600656";
 
 const productos = [
-  { id: 1, nombre: "Creatina Planitun 80 Servicios", precio: 40, imagen: "001.jpg", stock: true },
+  { id: 1, nombre: "Creatina Planitun 80 Servicios", precio: 40, imagen: "001.jpg", stock: false },
   { id: 2, nombre: "Creatina Dimatize 60 Servicios", precio: 40, imagen: "002.jpg", stock: true },
+  { id: 8, nombre: "Creatina Micronizada 120 Servicios", precio: 55, imagen: "014.jpg", stock: true },
+  { id: 8, nombre: "Creatina Micronizada 60 Servicios", precio: 30, imagen: "008.jpg", stock: true },
   { id: 3, nombre: "Ashwagandha 60 Servicios", precio: 20, imagen: "003.jpg", stock: false },
   { id: 4, nombre: "Test Booster 120 Servicios", precio: 35, imagen: "004.jpg", stock: false },
   { id: 5, nombre: "Pre Entrenamiento NITROSURGE - 30 servicios", precio: 35, imagen: "005.jpg", stock: false },
   { id: 6, nombre: "Pre Entrenamiento the curse - 30 servicios", precio: 30, imagen: "006.jpg", stock: false },
   { id: 7, nombre: "Kaged Sport Pre Workout", precio: 35, imagen: "007.jpg", stock: false },
-  { id: 8, nombre: "Creatina Micronizada 60 Servicios", precio: 30, imagen: "008.jpg", stock: true },
   { id: 9, nombre: "Pre Entrenamiento NITROSURGE - 10 servicios", precio: 25, imagen: "009.jpg", stock: false },
   { id: 10, nombre: "Creatina Monohidratada JACKED FACTORY - 30 servicios", precio: 25, imagen: "010.jpg", stock: false },
   { id: 11, nombre: "Creatina Monohidratada the curse - 60 servicios", precio: 35, imagen: "011.jpg", stock: false },
   { id: 12, nombre: "Creatina Monohidratad Rule one - 30 servicios", precio: 25, imagen: "012.jpg", stock: false },
-  { id: 13, nombre: "Creatina Monohidratad nutrex - 60 servicios", precio: 30, imagen: "013.jpg", stock: false },
-  { id: 8, nombre: "Creatina Micronizada 120 Servicios", precio: 55, imagen: "014.jpg", stock: true }
+  { id: 13, nombre: "Creatina Monohidratad nutrex - 60 servicios", precio: 30, imagen: "013.jpg", stock: false }
 ];
 
 let carrito = [];
