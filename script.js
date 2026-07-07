bb0ce76aa9e2f713dcd8a410bd53ae886cdd1777
SuplementosCubias <cubiasonline@gmail.com>
2026-07-07 17:13:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,131 @@
+const telefono = "50376600656";
+
+const productos = [
+    {
+        id: 1,
+        nombre: "Creatina Planitun 80 Servicios",
+        precio: 40,
+        imagen: "001.jpg",
+        stock: true
+    },
+    {
+        id: 2,
+        nombre: "Creatina Micronizada 120 Servicios",
+        precio: 55,
+        imagen: "014.jpg",
+        stock: true
+    },
+    {
+        id: 3,
+        nombre: "Creatina Micronizada 60 Servicios",
+        precio: 30,
+        imagen: "008.jpg",
+        stock: true
+    }
+];
+
+let carrito = [];
+
+/* CATÁLOGO */
 
 const catalogo = document.getElementById("catalogo");
 
-catalogo.innerHTML = `
-    <div class="producto">
-        001.jpg" alt="Producto">
-        <h3>Creatina Planitun 80 Servicios</h3>
-        <p>$40</p>
-        <button>Agregar</button>
-    </div>
-`;
+productos.forEach(producto => {
+
+    catalogo.innerHTML += `
+        <div class="producto">
+
+            ${producto.imagen}
+
+            <h3>${producto.nombre}</h3>
+
+            <p>$${producto.precio}</p>
+
+            <button onclick="agregarAlCarrito(${producto.id})">
+                Agregar
+            </button>
+
+        </div>
+    `;
+});
+
+/* CARRITO */
+
+function agregarAlCarrito(id){
+
+    const producto =
+        productos.find(p => p.id === id);
+
+    carrito.push(producto);
+
+    actualizarCarrito();
+}
+
+function actualizarCarrito(){
+
+    const divCarrito =
+        document.getElementById("carrito");
+
+    let total = 0;
+
+    divCarrito.innerHTML = "";
+
+    carrito.forEach((producto,index)=>{
+
+        total += producto.precio;
+
+        divCarrito.innerHTML += `
+            <p>
+                ${producto.nombre}
+                - $${producto.precio}
+
+                <button onclick="eliminar(${index})">
+                    ❌
+                </button>
+            </p>
+        `;
+    });
+
+    document.getElementById("total").innerText =
+        "Total: $" + total;
+}
+
+function eliminar(index){
+
+    carrito.splice(index,1);
+
+    actualizarCarrito();
+}
+
+/* WHATSAPP */
+
+function enviarWhatsApp(){
+
+    if(carrito.length === 0){
+        alert("El carrito está vacío");
+        return;
+    }
+
+    let mensaje =
+        "Hola, deseo realizar el siguiente pedido:\n\n";
+
+    let total = 0;
+
+    carrito.forEach(producto => {
+
+        mensaje +=
+            `• ${producto.nombre} - $${producto.precio}\n`;
+
+        total += producto.precio;
+    });
+
+    mensaje += `\nTotal: $${total}`;
+
+    window.open(
+        `https://wa.me/${telefono}?text=${encodeURIComponent(mensaje)}`,
+        "_blank"
+    );
+}
+
+    `;
+});
