d7d5f50871ae11c6412f780fc6dd902559ca74ae
SuplementosCubias <cubiasonline@gmail.com>
2026-07-07 18:09:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ productos.forEach(producto => {
     catalogo.innerHTML += `
         <div class="producto">
 
-            ${producto.imagen}.nombre}">
+            .imagen}" alt="${producto.nombre}">
 
             <h3>${producto.nombre}</h3>
 
